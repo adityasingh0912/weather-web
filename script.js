61a8d603c6c94c2ad5008003fa029d853e7e2a21
adityasingh0912 <singh0912aditya@gmail.com>
2024-05-24 23:07:00 +0530

    weather

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-async function fetchWeather() {
-    const city = document.getElementById('city').value || 'Delhi';
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': '7e2ffc142fmsha838d7643a0ce98p18b774jsndf67bafb0eec',
-        'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-      }
-    };
-  
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result);
-    cloud_pct = result.cloud_pct;
-    temp = result.temp;
-    feels_like = result.feels_like;
-    humidity = result.humidity;
-    min_temp = result.min_temp;
-    max_temp = result.max_temp;
-    wind_speed = result.wind_speed;
-    wind_degrees = result.wind_degrees;
-    sunrise = result.sunrise;
-    sunset = result.sunset;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  document.getElementById('submit').addEventListener('click', fetchWeather);
+let button = document.querySelector(".button");
+let inputvalue = document.querySelector(".search-box");
+let temp = document.querySelector(".current .temp");
+let description = document.querySelector(".current .weather");
+let city = document.querySelector(".location .city");
+let date = document.querySelector(".location .date");
+
+button.addEventListener("click", function () {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=03a094de2fbed757402784c8ab602833`
+  )
+    .then((response) => response.json())
+    .then(displayData)
+    .catch((err) => alert("Please Enter Correct City Name"));
+});
+
+const displayData = (weather) => {
+  temp.innerHTML = `${Math.round(weather.main.temp)}°C`;
+  description.innerText = `${weather.weather[0].description}`;
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+  let now = new Date();
+  date.innerText = dateBuilder(now);
+};
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
